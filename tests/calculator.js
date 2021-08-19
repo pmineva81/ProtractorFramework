a11288d881294e7f7d906a68961202001e1435c2
pmineva81 <polkam@abv.bg>
2021-08-19 15:27:09 +0300

    my first commit

---
+++ tests/calculator.js
@@ -0,0 +1,23 @@
+let homepage = require('../pages/homepage')
+
+describe('demo calculator tests', function(){
+    it('addition test', function(){
+        homepage.getUrl('http://juliemr.github.io/protractor-demo/');
+        homepage.enterFirstNumber('4');
+        homepage.enterSecondNumber('5');
+        homepage.clickGo();
+        homepage.verifyResult('9');
+        browser.sleep(2000)
+    });
+
+
+    it('subjection test', function(){
+        homepage.getUrl('http://juliemr.github.io/protractor-demo/');
+        homepage.enterFirstNumber('4');
+        homepage.enterSecondNumber('5');
+        homepage.clickGo1();
+        homepage.verifyResult('9');
+        browser.sleep(2000)
+    });
+
+});
